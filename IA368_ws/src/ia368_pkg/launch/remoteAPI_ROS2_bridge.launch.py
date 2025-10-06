b3f34984cbbe1e7d4878636438b45f67d63ca756
Patric Moreto <p223083@dac.unicamp.br>
2025-10-06 00:35:44 -0300

    Remove namespace and node name to match the original scripts from the students

--- IA368_ws/src/ia368_pkg/launch/remoteAPI_ROS2_bridge.launch.py
+++ IA368_ws/src/ia368_pkg/launch/remoteAPI_ROS2_bridge.launch.py
@@ -5,30 +5,22 @@ def generate_launch_description():
     return LaunchDescription([
         Node(
             package='ia368_pkg',
-            namespace='autodocking',
             executable='battery_node',
-            name='battery_node',
             output='screen'
         ),
         Node(
             package='ia368_pkg',
-            namespace='autodocking',
             executable='bumper_and_velocity_node',
-            name='bumper_and_velocity_node',
             output='screen'
         ),
         Node(
             package='ia368_pkg',
-            namespace='autodocking',
             executable='charging_base_node',
-            name='charging_base_node',
             output='screen'
         ),
         Node(
             package='ia368_pkg',
-            namespace='autodocking',
             executable='docking_node',
-            name='docking_node',
             output='screen'
         )
     ])
