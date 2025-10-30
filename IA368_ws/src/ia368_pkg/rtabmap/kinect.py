ad0899d1b9f936251d11473f1a0e069ac4227bed
Patric Moreto <patric.moreto@eldorado.org.br>
2025-10-29 22:22:29 -0300

    Separate kinect_node publishers in 3 timers, as suggested in https://github.com/cesarbds/IA368ii/commit/a7f591fb1e3aa30e896eee685eab8c6c6299ed16

--- IA368_ws/src/ia368_pkg/rtabmap/kinect.py
+++ IA368_ws/src/ia368_pkg/rtabmap/kinect.py
@@ -6,9 +6,11 @@ from sensor_msgs.msg import Image
 from sensor_msgs.msg import CameraInfo
 from sensor_msgs.msg import RegionOfInterest
 # Coppelia ZeroMQ Remote API
-from coppeliasim_zmqremoteapi_client import *
+from coppeliasim_zmqremoteapi_client import RemoteAPIClient
 
 class KinectNode(Node):
+    info_msg = None
+
     def __init__(self):
         super().__init__('kinect_node')
         
@@ -32,12 +34,44 @@ class KinectNode(Node):
             self.get_logger().error(f'Failed to connect to CoppeliaSim: {e}')
             return
         
+        # Prepare info_msg with default settings
+        self.populate_info_msg()
+        
         # Timer to publish kinect data every 10Hz
-        self.timer = self.create_timer(0.1, self.publish_camera)
+        self.timer_rgb = self.create_timer(0.1, self.publish_camera_rgb)
+        self.timer_depth = self.create_timer(0.1, self.publish_camera_depth)
+        self.timer_info = self.create_timer(0.1, self.publish_camera_info)
         
         self.get_logger().info('ROS2 → CoppeliaSim Kinect node started.')
     
-    def publish_camera(self):
+    def populate_info_msg(self):
+        self.info_msg = CameraInfo()
+        self.info_msg.roi = RegionOfInterest()
+
+        # Fill message
+        self.info_msg.header.frame_id = "kinect"
+
+        resolution = self.sim.getVisionSensorResolution(self.depthCam)
+        if resolution is not None:
+            self.info_msg.height = resolution[1]
+            self.info_msg.width = resolution[0]
+        else:
+            self.get_logger().warn('Kinect camera info not available')
+
+        self.info_msg.distortion_model = 'plumb_bob'
+        self.info_msg.d = [0.262383,-0.953104,-0.005358,0.002628,1.163314]
+        self.info_msg.k = [517.306408, 0.0, 318.643040, 0.0, 516.469215, 255.313989, 0.0, 0.0, 1.0]
+        self.info_msg.p = [517.306408, 0.0, 318.643040, 0.0, 0.0, 516.469215, 255.313989, 0.0, 0.0, 0.0, 1.0, 0.0]
+        self.info_msg.binning_x = 0
+        self.info_msg.binning_y = 0
+
+        self.info_msg.roi.x_offset = 0
+        self.info_msg.roi.y_offset = 0
+        self.info_msg.roi.height = 0
+        self.info_msg.roi.width = 0
+        self.info_msg.roi.do_rectify = False     
+
+    def publish_camera_rgb(self):
         #try:
         # Get vision sensor RGB image from CoppeliaSim
         data, resolution = self.sim.getVisionSensorImg(self.colorCam)
@@ -62,14 +96,15 @@ class KinectNode(Node):
         else:
             self.get_logger().warn('Kinect RGB image not found')
     
+    def publish_camera_depth(self):
         # Get vision sensor RGB depth from CoppeliaSim
         data = self.sim.getVisionSensorDepthBuffer(self.depthCam+self.sim.handleflag_codedstring)
+        
         resolution, nearClippingPlane = self.sim.getObjectFloatParameter(self.depthCam,self.sim.visionfloatparam_near_clipping)
         resolution, farClippingPlane = self.sim.getObjectFloatParameter(self.depthCam,self.sim.visionfloatparam_far_clipping)
         nearClippingPlane = nearClippingPlane*1000 # we want mm
         farClippingPlane = farClippingPlane*1000 # we want mm
         data = self.sim.transformBuffer(data,self.sim.buffer_float,farClippingPlane-nearClippingPlane,nearClippingPlane,self.sim.buffer_uint16)
-        resolution = self.sim.getVisionSensorResolution(self.depthCam)
         
         if data is not None:
             # Create image message
@@ -78,45 +113,24 @@ class KinectNode(Node):
             # Fill image data
             depth_msg.header.stamp = self.get_clock().now().to_msg()
             depth_msg.header.frame_id = "kinect"
-            depth_msg.height = resolution[1]
-            depth_msg.width = resolution[0]
+            depth_msg.height = self.info_msg.height
+            depth_msg.width = self.info_msg.width
             depth_msg.encoding = '16UC1'
             depth_msg.is_bigendian = 0
-            depth_msg.step = resolution[0]*2
+            depth_msg.step = depth_msg.width * 2
             depth_msg.data = data
-            
+
             # Publish the message
             self.depth_publisher.publish(depth_msg)
         else:
             self.get_logger().warn('Kinect depth image not found')
 
-        if resolution is not None:
-            # Publish camera info
-            info_msg = CameraInfo()
-            info_msg.roi = RegionOfInterest()
+    def publish_camera_info(self):
+        # Fill message
+        self.info_msg.header.stamp = self.get_clock().now().to_msg()
 
-            # Fill message
-            info_msg.header.stamp = self.get_clock().now().to_msg()
-            info_msg.header.frame_id = "kinect"
-            info_msg.height = resolution[1]
-            info_msg.width = resolution[0]
-            info_msg.distortion_model = 'plumb_bob'
-            info_msg.d = [0.262383,-0.953104,-0.005358,0.002628,1.163314]
-            info_msg.k = [517.306408, 0.0, 318.643040, 0.0, 516.469215, 255.313989, 0.0, 0.0, 1.0]
-            info_msg.p = [517.306408, 0.0, 318.643040, 0.0, 0.0, 516.469215, 255.313989, 0.0, 0.0, 0.0, 1.0, 0.0]
-            info_msg.binning_x = 0
-            info_msg.binning_y = 0
-            
-            info_msg.roi.x_offset = 0
-            info_msg.roi.y_offset = 0
-            info_msg.roi.height = 0
-            info_msg.roi.width = 0
-            info_msg.roi.do_rectify = False
-
-            # Publish the message
-            self.camerainfo_publisher.publish(info_msg)
-        else:
-            self.get_logger().warn('Kinect camera info not available')
+        # Publish the message
+        self.camerainfo_publisher.publish(self.info_msg)
 
 def main(args=None):
     rclpy.init(args=args)
