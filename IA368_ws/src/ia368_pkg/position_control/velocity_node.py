153e708ffe4a945a45c1527b59f398b2c43424c2
Patric Moreto <p223083@dac.unicamp.br>
2025-10-31 21:32:29 -0300

    Update velocity node including flag with status from msg callback

--- IA368_ws/src/ia368_pkg/position_control/velocity_node.py
+++ IA368_ws/src/ia368_pkg/position_control/velocity_node.py
@@ -1,14 +1,15 @@
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
-from coppeliasim_zmqremoteapi_client import *
+from coppeliasim_zmqremoteapi_client import RemoteAPIClient
 
 class SubscriberVelocity(Node):
     def __init__(self):
         super().__init__('subscriber_velocity')
-        self.subscription = self.create_subscription(Twist, 'myRobot/cmd_vel', self.callback, 10)
+        self.subscription = self.create_subscription(Twist, 'myRobot/cmd_vel', self.msg_callback, 10)
         self.wheel_base = 0.331  # meters
         self.radius = 0.195/2    # meters
+        timer_interval = 0.4     # seconds
          # Connect to CoppeliaSim
         try:
             self.client = RemoteAPIClient()
@@ -25,14 +26,26 @@ class SubscriberVelocity(Node):
             self.get_logger().error(f'Failed to connect to CoppeliaSim: {e}')
             return
 
-    def callback(self, msg):
+        # Create flag for the message subscription callback status
+        self.msg_flag = False
+        # Create timer to send velocity to robot
+        self.timer = self.create_timer(timer_interval, self.send_velocity)
+
+    def msg_callback(self, msg):
         linVel = msg.linear.x
         rotVel = msg.angular.z
         # Inverse kinematics for differential wheeled robot
-        rightVel = (linVel + self.wheel_base /2 * rotVel)/self.radius
-        leftVel  = (linVel - self.wheel_base /2 * rotVel)/self.radius
-        self.sim.setJointTargetVelocity(self.rightMotor,rightVel)
-        self.sim.setJointTargetVelocity(self.leftMotor,leftVel)
+        self.rightVel = (linVel + self.wheel_base /2 * rotVel)/self.radius
+        self.leftVel  = (linVel - self.wheel_base /2 * rotVel)/self.radius
+        self.msg_flag = True
+
+    def send_velocity(self):
+        # If there is not a new message, send 0 velocity
+        if (self.msg_flag == False):
+            self.rightVel, self.leftVel = 0, 0
+        self.sim.setJointTargetVelocity(self.rightMotor, self.rightVel)
+        self.sim.setJointTargetVelocity(self.leftMotor, self.leftVel)
+        self.msg_flag = False
 
 def main(args=None):
     rclpy.init(args=args)
@@ -44,7 +57,6 @@ def main(args=None):
     finally:
         node.destroy_node()
         rclpy.shutdown()
-
 
 if __name__ == '__main__':
     main()
